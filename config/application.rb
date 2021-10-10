bd52379e4adb18157b1322411797e8af1b54051f
EC2 Default User <ec2-user@ip-172-31-32-49.ap-northeast-1.compute.internal>
2021-10-10 01:00:53 +0000

    2-33

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,14 @@ module KyotoApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+     # アプリケーションが対応している言語のホワイトリスト
+    config.i18n.available_locales = %i[ja en]
+     # デフォルトの言語設定
+    config.i18n.default_locale = :ja
+     # 言語ファイル階層ごとに設定するための記述
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+     # Please use `Rails.root.join('path/to')` instead.と言われたら、下記のようにする。
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -26,7 +34,3 @@ module KyotoApp
     end
   end
 end
-config.i18n.available_locales = %i[ja en]
-config.i18n.default_locale = :ja
-config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
