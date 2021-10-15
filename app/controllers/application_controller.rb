1c6c582d74898080c64f2f94b6093b3faab4fa0a
EC2 Default User <ec2-user@ip-172-31-32-49.ap-northeast-1.compute.internal>
2021-10-15 01:49:02 +0000

    2-37

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,16 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path # ログアウト後に遷移するpathを設定
   end
+
+  def create
+   user = User.find_by(email: params[:session][:email].downcase)
+   if user && user.authenticate(params[:session][:password])
+     log_in user
+     params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+     redirect_to user
+   else
+     flash.now[:danger] = 'Invalid email/password combination'
+     render 'new'
+   end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,17 @@
 module ApplicationHelper
+    # 記憶トークンcookieに対応するユーザーを返す
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+  
   def bs4_bgcolor_for(flash_key)
     case flash_key
     when "success"
