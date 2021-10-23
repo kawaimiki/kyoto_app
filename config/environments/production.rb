643e048e5f6a1302002c983a9528aa88ca65b74f
EC2 Default User <ec2-user@ip-172-31-32-49.ap-northeast-1.compute.internal>
2021-10-23 03:12:30 +0000

    2-49

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,15 +91,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: "https://sleepy-retreat-66493.herokuapp.com"}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "https://sleepy-retreat-66493.herokuapp.com" }
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-  :enable_starttls_auto => true,
-  :address => ENV['address'],
-  :port => 587,
-  :domain => 'smtp.gmail.com',
-  :user_name => ENV['address'], #gmailアドレス
-  :password => ENV['password'], #gmailパスワード
-  :authentication => 'login',
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
   }
-
 end
